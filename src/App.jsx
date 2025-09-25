7f25603d397ec0ba470f961d88bafbf6ea653927
Shifana <shifana1502@gmail.com>
2025-09-25 11:56:01 +0530

    Initial commit: React + Tailwind project setup

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gradient-to-r from-purple-500 to-pink-500">
+      <h1 className="text-5xl font-bold text-white">Hello Tailwind + React 🎉</h1>
+    </div>
+  );
+}
